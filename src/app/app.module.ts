c6913b1fe204c5446b85746cd01b840e8f4f8b48
burcineren <burcin.ern969@gmail.com>
2025-02-28 18:37:04 +0300

    feat: credit aplication state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './core/shared/shared.module';
+import { NgxsModule } from '@ngxs/store';
+import { CreditApplicationState } from './core/store/credits-state/credit-aplication.state';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,6 +17,12 @@ import { SharedModule } from './core/shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    HttpClientModule,
+    NgxsModule.forRoot([CreditApplicationState]),
+    NgxsLoggerPluginModule.forRoot(),
+    NgxsReduxDevtoolsPluginModule.forRoot()
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
